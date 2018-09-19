8a766790bb604da70af5c55ee2d04f100220d6f4
TrumanCai <007memory@gmail.com>
2018-09-20 00:03:05 +0800

    fix unit test

--- src/index.test.js
+++ src/index.test.js
@@ -3,9 +3,24 @@ const starWars = require('./index');
 const pool = require('./starwars-names');
 
 describe("starwars-names", function() {
-    it('should be true', function() {
-        let name = starWars.random();
-        let source = Array.from(pool);
-        expect(source.indexOf(name)> -1).to.be.true;
+    describe('all', function () {
+        it('should be array of strings', function () {
+            expect(starWars.all).to.satisfy(isArrayOfStrings);
+        });
+        function isArrayOfStrings(array) {
+            return array.every(function (item) {
+                return typeof item === "string";
+            });
+        }
+
+        it('should be 4-LOM', function () {
+            expect(starWars.all).to.include("4-LOM");
+        });
+    });
+    describe('random', function () {
+        it('should return a random item from the random all', function () {
+            const randomItem = starWars.random();
+            expect(starWars.all).to.contain(randomItem);
+        });
     });
 });
