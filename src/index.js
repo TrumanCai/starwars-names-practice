e7d37ade2e1db8f22c8c9210d511b30b92aa4687
TrumanCai <007memory@gmail.com>
2018-09-21 02:26:05 +0800

    feat(random): add ability to get an array of starwars names
    if you pass a number to the random function, you'll receive an array with that number of random.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const uniqueRandomArray = require("unique-random-array");
 const starWarsNames = require("./starwars-names.json");
+const getRandomItem = uniqueRandomArray(starWarsNames);
 module.exports = {
   all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames)
+  random: random
 };
+
+function random(n) {
+  if(undefined === n) {
+      return getRandomItem();
+  } else {
+    let randomItems = [];
+    for(let i = 0; i < n; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -2,25 +2,37 @@ const expect = require('chai').expect;
 const starWars = require('./index');
 const pool = require('./starwars-names');
 
-describe("starwars-names", function() {
-    describe('all', function () {
-        it('should be array of strings', function () {
-            expect(starWars.all).to.satisfy(isArrayOfStrings);
-        });
-        function isArrayOfStrings(array) {
-            return array.every(function (item) {
-                return typeof item === "string";
-            });
-        }
+describe("starwars-names", function () {
+  describe('all', function () {
+    it('should be array of strings', function () {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+    });
 
-        it('should be 4-LOM', function () {
-            expect(starWars.all).to.include("4-LOM");
-        });
+    function isArrayOfStrings(array) {
+      return array.every(function (item) {
+        return typeof item === "string";
+      });
+    }
+
+    it('should be 4-LOM', function () {
+      expect(starWars.all).to.include("4-LOM");
     });
-    describe('random', function () {
-        it('should return a random item from the random all', function () {
-            const randomItem = starWars.random();
-            expect(starWars.all).to.contain(randomItem);
-        });
+  });
+  describe('random', function () {
+    it('should return a random item from the random all', function () {
+      const randomItem = starWars.random();
+      expect(starWars.all).to.contain(randomItem);
     });
+    it('should return an array of random items if pass a number', function () {
+      const randomItems = starWars.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function (item) {
+        console.log(item);
+        expect(starWars.all).to.contains(item);
+        expect(starWars.all).to.contain(item);
+        expect(starWars.all).to.includes(item);
+        expect(starWars.all).to.include(item);
+      });
+    });
+  });
 });
