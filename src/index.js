5c2a7050a92b7192a6492a0c5575c7338c71a21a
TrumanCai <007memory@gmail.com>
2018-10-07 01:40:06 +0800

    refactor(source, test): Add es6 support, add es6 support to tests using mocha and babel.
    It takes me a long time to fix 'zero coverage' issue, see also,
    https://github.com/istanbuljs/babel-plugin-istanbul/issues/169,
    https://github.com/istanbuljs/babel-plugin-istanbul/issues/131,
    https://github.com/istanbuljs/babel-plugin-istanbul/issues/128. finally it solved.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-const uniqueRandomArray = require("unique-random-array");
-const starWarsNames = require("./starwars-names.json");
-const getRandomItem = uniqueRandomArray(starWarsNames);
+import uniqueRandomArray from 'unique-random-array'
+import starWarsNames from './starwars-names.json'
+const getRandomItem = uniqueRandomArray(starWarsNames)
 module.exports = {
   all: starWarsNames,
   random: random,
   containsInsensitive: getItems
-};
+}
 
-function random(n) {
-  if(undefined === n) {
-      return getRandomItem();
+function random (n) {
+  if (undefined === n) {
+    return getRandomItem()
   } else {
-    let randomItems = [];
-    for(let i = 0; i < n; i++) {
-      randomItems.push(getRandomItem());
+    let randomItems = []
+    for (let i = 0; i < n; i++) {
+      randomItems.push(getRandomItem())
     }
-    return randomItems;
+    return randomItems
   }
 }
 
-function getItems(seed) {
-  if(void 0 === seed) {
-    return void 0;
+function getItems (seed) {
+  if (void 0 === seed) {
+    return void 0
   } else {
-    let items = [];
-    starWarsNames.forEach(function(item) {
+    let items = []
+    starWarsNames.forEach(function (item) {
       if (item.includes(seed)) {
-        items.push(item);
+        items.push(item)
       }
-    });
-    return items;
+    })
+    return items
   }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,49 +1,49 @@
-const expect = require('chai').expect;
-const starWars = require('../src/index');
+import { expect } from 'chai'
+import starWars from '../src/index'
 
-describe("starwars-names", function () {
+describe('starwars-names', function () {
   describe('all', function () {
     it('should be array of strings', function () {
-      expect(starWars.all).to.satisfy(isArrayOfStrings);
-    });
+      expect(starWars.all).to.satisfy(isArrayOfStrings)
+    })
 
-    function isArrayOfStrings(array) {
+    function isArrayOfStrings (array) {
       return array.every(function (item) {
-        return typeof item === "string";
-      });
+        return typeof item === 'string'
+      })
     }
 
     it('should be 4-LOM', function () {
-      expect(starWars.all).to.include("4-LOM");
-    });
-  });
+      expect(starWars.all).to.include('4-LOM')
+    })
+  })
   describe('random', function () {
     it('should return a random item from the random all', function () {
-      const randomItem = starWars.random();
-      expect(starWars.all).to.contain(randomItem);
-    });
+      const randomItem = starWars.random()
+      expect(starWars.all).to.contain(randomItem)
+    })
     it('should return an array of random items if pass a number', function () {
-      const randomItems = starWars.random(3);
-      expect(randomItems).to.have.length(3);
+      const randomItems = starWars.random(3)
+      expect(randomItems).to.have.length(3)
       randomItems.forEach(function (item) {
-        console.log(item);
-        expect(starWars.all).to.contains(item);
-        expect(starWars.all).to.contain(item);
-        expect(starWars.all).to.includes(item);
-        expect(starWars.all).to.include(item);
-      });
-    });
-  });
+        console.log(item)
+        expect(starWars.all).to.contains(item)
+        expect(starWars.all).to.contain(item)
+        expect(starWars.all).to.includes(item)
+        expect(starWars.all).to.include(item)
+      })
+    })
+  })
   describe('containsInsensitive', function () {
     it('should return all items contain M/m from all', function () {
-      const allItems = starWars.containsInsensitive('m');
-      allItems.forEach(function( item ) {
-        expect(starWars.all).to.contains(item);
-      });
-    });
+      const allItems = starWars.containsInsensitive('m')
+      allItems.forEach(function (item) {
+        expect(starWars.all).to.contains(item)
+      })
+    })
     it('should return null when search nothing', function () {
-      const allItems = starWars.containsInsensitive();
-      expect(void 0).to.be.equal(allItems);
-    });
-  });
-});
+      const allItems = starWars.containsInsensitive()
+      expect(void 0).to.be.equal(allItems)
+    })
+  })
+})
